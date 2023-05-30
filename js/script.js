ebcc6ba78e931986718f25f182ed30a4ed65ab06
GLCVLL <g.ioele96@outlook.it>
2023-05-30 17:27:06 +0200

    added loop

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log('JS OK');
+
+
+// PRENDO L'ELEMENTO DAL DOM
+
+const blackboardElement= document.querySelector('.blackboard');
+
+// PREPARIAMO IL CICLO
+
+for(let i = 1; i <= 100; i++){
+    console.log(i);
+    blackboardElement.innerText += i + ' '; 
+}
