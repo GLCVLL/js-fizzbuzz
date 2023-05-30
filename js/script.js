b4b81c7dacb2854dcb5471296b2227e0b05dfa19
GLCVLL <g.ioele96@outlook.it>
2023-05-30 18:32:09 +0200

    added multiple 3 and 5

--- js/script.js
+++ js/script.js
@@ -7,7 +7,17 @@ const blackboardElement= document.querySelector('.blackboard');
 
 // PREPARIAMO IL CICLO
 
-for(let i = 1; i <= 100; i++){
-    console.log(i);
-    blackboardElement.innerText += i + ' '; 
+blackboardElement.innerText = '';
+
+for (let i = 1; i <= 100; i++) {
+    //scambio con fizzbuzz
+  if (i % 3 === 0 && i % 5 === 0) {
+    blackboardElement.innerText += "fizzbuzz";
+  } else if (i % 3 === 0) {
+    blackboardElement.innerText += "fizz"; // scambio con fizz
+  } else if (i % 5 === 0) {
+    blackboardElement.innerText += "buzz"; //scambio con buzz
+  } else {
+    blackboardElement.innerText += i;
+  }
 }
